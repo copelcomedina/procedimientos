338aa6b5fe305f9996c63056e70efa6a7064fb23
Jorge Medina <medinaj@copelco.com.ar>
2026-05-07 10:12:39 -0300

    fix(auth): eliminamos (opcional)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,12 @@ module.exports = {
   apps: [{
     name: 'procedimientos',
     script: '.next/standalone/server.js',
-    cwd: '/var/www/rrhh.copelco.com.ar/apps/procedimientos',
+    cwd: '/var/www/rrhh.copelco.com.ar/public_html/procedimientos',
     env: {
       NODE_ENV: 'production',
       PORT: 3001,
-      DB_PATH: '/var/www/rrhh.copelco.com.ar/apps/procedimientos/data/procedimientos.sqlite',
-      UPLOAD_DIR: '/var/www/rrhh.copelco.com.ar/apps/procedimientos/uploads',
+      DB_PATH: '/var/www/rrhh.copelco.com.ar/public_html/procedimientos/data/procedimientos.sqlite',
+      UPLOAD_DIR: '/var/www/rrhh.copelco.com.ar/public_html/procedimientos/uploads',
       JWT_SECRET: 'copelco-cambia-esta-clave-2026',
       HOSTNAME: '0.0.0.0',
     }
